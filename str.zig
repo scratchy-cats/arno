501a4aae5a3dfdc35e392c82ab45122a3ea94b2b
bluebrown <rainbowstack@gmail.com>
2024-05-29 21:09:38 +0200

    fix(str): hex exclsive reverse
    Signed-off-by: bluebrown <rainbowstack@gmail.com>

--- str.zig
+++ str.zig
@@ -1,11 +1,3 @@
-pub fn reverse(s: []const u8) []const u8 {
-    var r: [16]u8 = undefined;
-    for (0..s.len) |i| {
-        r[i] = s[s.len - i - 1];
-    }
-    return r[0..s.len];
-}
-
 pub fn btoax(val: u64) []const u8 {
     var res: [18]u8 = undefined;
     var i: u32 = 0;
@@ -13,21 +5,34 @@ pub fn btoax(val: u64) []const u8 {
 
     while (v > 0) {
         const d = v % 16;
-        res[i] = hexd(@truncate(d));
+        res[i] = x_digit(@truncate(d));
         v /= 16;
+        i += 1;
+    }
+
+    if (i == 0) {
+        res[i] = '0';
         i += 1;
     }
 
     res[i] = 'x';
     res[i + 1] = '0';
 
-    return reverse(res[0 .. i + 2]);
+    return x_reverse(res[0 .. i + 2]);
 }
 
-inline fn hexd(d: u8) u8 {
+inline fn x_digit(d: u8) u8 {
     if (d < 10) {
         return '0' + d;
     } else {
         return 'a' + (d - 10);
     }
+}
+
+fn x_reverse(s: []const u8) []const u8 {
+    var r: [18]u8 = undefined;
+    for (0..s.len) |i| {
+        r[i] = s[s.len - i - 1];
+    }
+    return r[0..s.len];
 }
